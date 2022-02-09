ed07a6fb15a3dfb9971c44f3a15ef2a198dca9d3
ivekhov <ivekhov@gmail.com>
2022-02-10 00:45:37 +0300

    Update homework with selecting tasks.

--- DE-101/Module02/homework.sql
+++ DE-101/Module02/homework.sql
@@ -1,5 +1,5 @@
 /***
-Идеи для создания дашборда отчета:
+1. Идеи для создания дашборда отчета:
 
 Overview (обзор ключевых метрик)
 Total Sales
@@ -84,7 +84,20 @@ select
 	,  round(sum(sales),1) as "Total Sales"
 from orders
 group by 1, 2, 3, 4
-order by 5 desc;
+order by 1 asc, 2 asc , 3 asc, 4 asc;
+
+
+
+-- Sales by  category by Months
+select	
+	category
+	,  extract('year' from order_date) as "Year"	
+	,  extract('month' from order_date)  as "Month"
+	,  round(sum(sales),1) as "Total Sales"
+from orders
+group by 1, 2, 3
+order by 1 asc, 2 asc , 3 asc;
+
 
 
 -- Average Monthly Sales by segment and category 
@@ -105,9 +118,6 @@ select
 from orders
 group by 1, 2
 order by 3 desc;
-
-
-
 
 
 
@@ -167,4 +177,77 @@ select
 from orders
 group by 1
 order by 3 desc;
+
+
+
+
+/***
+2. Product Dashboard (Продуктовые метрики)
+
+Sales by Product Category over time (Продажи по категориям)
+Customer Analysis
+Sales and Profit by Customer
+Customer Ranking
+Sales per region
+
+***/
+
+
+-- Sales by  category by Months
+select	
+	category as "Category"
+	,  extract('year' from order_date) as "Year"	
+	,  extract('month' from order_date)  as "Month"
+	,  round(sum(sales),1) as "Total Sales"
+from orders
+group by 1, 2, 3
+order by 1 asc, 2 asc , 3 asc;
+
+
+-- Sales by subcategory by Months
+select	
+	subcategory as "Subcategory"
+	,  extract('year' from order_date) as "Year"	
+	,  extract('month' from order_date)  as "Month"
+	,  round(sum(sales),1) as "Total Sales"
+from orders
+group by 1, 2, 3
+order by 1 asc, 2 asc , 3 asc;
+
+-- Sales and Profit by Customer
+
+select 
+	distinct customer_id 
+	, customer_name
+	, round(sum(sales)) as "Total Sales"
+	, round(sum(profit)) as "Total Profit"
+from orders o 
+group by 1, 2
+order by 3 desc, 4 desc 
+
+
+
+-- Customer Ranking
+
+select 
+	distinct customer_id 
+	, customer_name
+	, rank() over (
+					order by sales desc
+					)
+from orders o 
+order by 3 asc 
+
+
+-- Sales per region
+select 
+	distinct region 
+	, round(sum(sales)) as "Total Sales"
+from orders o 
+group by 1
+order by 2 desc
+
+
+
+
 
